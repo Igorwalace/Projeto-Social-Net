3c2d192d6c046a4a425c1d2903e0c0e5a65d8442
igoor <igorwallacy3@gmail.com>
2024-06-03 14:18:37 -0300

    new env

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,40 +1,8 @@
-//next/react
 import React from 'react'
-import { auth } from '@/services/auth'
-import Image from 'next/image'
 
-//pages
-import Button_Logout from '../componentes/button-logout'
-import Link from 'next/link'
-
-const Home = async () => {
-
-  const session: any = await auth()
-
+const Home = () => {
   return (
-    <div className='flex items-center justify-center gap-2 flex-col' >
-      <div>Home</div>
-      <Button_Logout />
-      {
-        session &&
-        <>
-          <div className="border-2 border-blue-900 p-2 rounded-xl text-center space-y-3">
-            <h1>{session.user.name}</h1>
-            <h1>{session.user.email}</h1>
-            <h1>{session?.user?.userName}</h1>
-            <Image
-              className='max-w-[200px] max-h-[200px] rounded-xl'
-              quality={100}
-              src={session?.user?.image}
-              alt={session?.user?.name}
-              width={500}
-              height={500}
-            />
-          </div>
-        </>
-      }
-      <Link href='/pages/createUser' >Criar User</Link>
-    </div>
+    <div>Home</div>
   )
 }
 
