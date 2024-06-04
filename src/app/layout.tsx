9751d73bc3dc21c38a45702491adfdeeb1fba2ee
igoor <igorwallacy3@gmail.com>
2024-06-04 12:20:38 -0300

    color scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scrollbar-none md:scrollbar-thin scrollbar-track-white scrollbar-thumb-[var(--main)]" >
+    <html lang="en" className="scrollbar-none md:scrollbar-thin scrollbar-track-white scrollbar-thumb-slate-900" >
       <body className={`${poppins.className}`}>{children}</body>
     </html>
   );
