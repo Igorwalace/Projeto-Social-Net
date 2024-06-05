7876fe380e2d3aa9d63cd5a22cdae21ab4970296
igoor <igorwallacy3@gmail.com>
2024-06-04 21:01:45 -0300

    scrollbar none

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scrollbar-none md:scrollbar-thin scrollbar-track-white scrollbar-thumb-slate-900" >
+    <html lang="en" className="scrollbar-none md:scrollbar-thin md:scrollbar-track-white md:scrollbar-thumb-slate-900" >
       <body className={`${poppins.className}`}>{children}</body>
     </html>
   );
