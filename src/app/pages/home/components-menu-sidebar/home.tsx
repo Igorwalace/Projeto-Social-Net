af7bd59d9728899766ca73a083944c36405a66b2
igoor <igorwallacy3@gmail.com>
2024-06-05 12:02:03 -0300

    link profile e home

--- src/app/pages/home/components-menu-sidebar/home.tsx
+++ src/app/pages/home/components-menu-sidebar/home.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 //icons lucide
@@ -5,12 +6,12 @@ import { HomeIcon } from 'lucide-react'
 
 const Home = () => {
     return (
-        <div className="flex items-center gap-3" >
+        <Link href='/' className="flex items-center gap-3" >
             <span><HomeIcon className="w-6 h-6" /></span>
             <div className="font-medium">
                 <h1>Home</h1>
             </div>
-        </div>
+        </Link>
   )
 }
 
--- src/app/pages/home/components/footer-mobile.tsx
+++ src/app/pages/home/components/footer-mobile.tsx
@@ -1,12 +1,13 @@
 //next
 import React from 'react'
+import Link from 'next/link'
 import { auth } from '@/services/auth'
 
 //shadcn
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
 
 //icons
-import { CirclePlus, Bookmark, HomeIcon, SearchIcon, SlidersHorizontal } from 'lucide-react'
+import { CirclePlus, Bookmark, HomeIcon, SearchIcon } from 'lucide-react'
 
 const Footer_Mobile = async () => {
 
@@ -16,11 +17,13 @@ const Footer_Mobile = async () => {
         <>
             <main className='bg-[var(--main)] text-white py-3 px-6' >
                 <header className='flex items-center justify-between max-w-6xl mx-auto' >
-                    <span><HomeIcon className="w-5 h-5" /></span>
+                    <Link href='/' ><HomeIcon className="w-5 h-5" /></Link>
                     <span><SearchIcon /></span>
-                    <Avatar className="w-8 h-8">
-                        <AvatarImage src={user?.user?.image || ''} />
-                    </Avatar>
+                    <Link href='/pages/profile' >
+                        <Avatar className="w-8 h-8">
+                            <AvatarImage src={user?.user?.image || ''} />
+                        </Avatar>
+                    </Link>
                     <span><Bookmark className="w-5 h-5" /></span>
                     <span><CirclePlus className="w-5 h-5" /></span>
                 </header>
